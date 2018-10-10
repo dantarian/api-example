0cee287da9a465246b57acfe74e05ede830be33d
Warren Jones <warren.jones@civica.co.uk>
2018-10-10 11:17:08 +0000

    Added initial server configuration.

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Configure app to use BodyParser to make working with POSTs easier
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// Set up routes
+var router = express.Router();
+router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the Widget API!' });
+});
+
+// Register routes
+app.use('/api/', router);
+
+// Start server
+app.listen(port);
+console.log("Listening on port " + port);
